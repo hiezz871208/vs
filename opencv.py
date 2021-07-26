ccb72f6b348a06a4911e144c0413ce1cd751c83c
hiezz871208 <hiezz871208@gmail.com>
2021-07-26 17:17:56 +0800

    first commit

---
+++ opencv.py
@@ -0,0 +1,10 @@
+import numpy as np
+import cv2 as cv
+img1=cv.imread('beibei.jpg',0)
+cv.imshow('beibei',img1)
+k=cv.waitKey(0)
+if k==27:
+    cv.destroyAllWindows()
+elif k == ord('s'):
+    cv.imwrite('messigray.png',img1)
+    cv.destroyAllWindows()
